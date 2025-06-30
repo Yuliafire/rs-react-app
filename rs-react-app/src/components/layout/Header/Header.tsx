f4cc63a8f5eefd278303b4eb326dd00df8cb0dc6
Yulia <linguavoice1@yahoo.com>
2025-06-30 10:48:11 +0300

    feat: add Footer and Header components

---
+++ rs-react-app/src/components/layout/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className={styles.header}>
+                <h1 className={styles.title}>Star Wars</h1>
+            </header>
+        )
+    }
+}
+
+export default  Header;
