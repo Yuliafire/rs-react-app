df6f946d613f878d1a2d8e5cd7095101af1206fb
Yulia <linguavoice1@yahoo.com>
2025-07-01 20:29:37 +0300

    refactor: fix pagination visibility by modifing global styles

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import Header from '../src/components/layout/Header/Header';
 import Footer from '../src/components/layout/Footer/Footer';
