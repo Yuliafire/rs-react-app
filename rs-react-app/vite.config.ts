fc86599f077d81ac42189dacd76c8f86d9420c88
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-07-15 21:46:45 +0300

    refactor: update vite.config with exclude lines

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,45 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-
-  
-// });
-
-
-
-
-// import { defineConfig, type UserConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-// import svgr from 'vite-plugin-svgr';
-
-// export default defineConfig(({ mode }) => {
-//   const config: UserConfig = {
-//     plugins: [react(), svgr()],
-//     build: {
-//       target: 'es2022',
-//     },
-//     test: {
-//       environment: 'jsdom',
-//       globals: true,
-//       setupFiles: './tests/setup.js',
-//       coverage: {
-//         provider: 'istanbul',
-//         reporter: ['text', 'json', 'html'],
-//         exclude: ['**/*.d.ts', '**/*.config.ts', 'tests/**', 'src/constants/**', 'src/types/**', 'dist', 'coverage'],
-//       },
-//     },
-//   };
-
-//   if (mode === 'production') {
-//     config.base = './';
-//   }
-
-//   return config;
-// });
-
 /// <reference types="vitest/config" />
 
 import { defineConfig, type UserConfig } from 'vite';
@@ -55,7 +13,7 @@ export default defineConfig(({ mode }) => {
     test: {
       environment: 'jsdom',
       globals: true,
-      setupFiles: './tests/setup.js',
+      setupFiles: './tests/setupTests.ts',
       coverage: {
         include: ['src/**/*.{js,jsx,ts,tsx}'],
         provider: 'istanbul',
@@ -74,7 +32,7 @@ export default defineConfig(({ mode }) => {
           'src/setupTests.{js,ts}',
           'src/**/*.d.ts'
         ],
-        thresholds: {  // ← Added your coverage thresholds here
+        thresholds: {
           global: {
             statements: 80,
             branches: 50,
