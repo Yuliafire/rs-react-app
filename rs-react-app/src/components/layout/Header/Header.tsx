479baa4d976e50cdc232f19e3650d29900baf9ed
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-07-20 00:05:24 +0300

    refactor: refactor Footer and Header into functional components

--- rs-react-app/src/components/layout/Header/Header.tsx
+++ rs-react-app/src/components/layout/Header/Header.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
 import styles from './Header.module.scss';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className={styles.header}>
-        <h1 className={styles.title}>Rick & Morty</h1>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Rick & Morty</h1>
+    </header>
+  );
+};
 
 export default Header;
