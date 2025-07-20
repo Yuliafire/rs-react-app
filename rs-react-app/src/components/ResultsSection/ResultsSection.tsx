082dd6badb847aab8efd84c286c78defc7e86512
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-07-20 18:33:48 +0300

    refactor: refactor Results section into Functional component

--- rs-react-app/src/components/ResultsSection/ResultsSection.tsx
+++ rs-react-app/src/components/ResultsSection/ResultsSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { CharacterDetails } from '../../types/types';
 import styles from './ResultsSection.module.scss';
 import Loader from '../ui/Loader/Loader';
@@ -10,46 +9,42 @@ interface ResultsSectionProps {
   error: string | null;
 }
 
-class ResultsSection extends React.Component<ResultsSectionProps> {
-  render() {
-    const { loading, error, results } = this.props;
-
-    if (loading) {
-      return (
-        <section
-          className={styles.resultsSection}
-          role="alert"
-          aria-live="polite"
-        >
-          <div className={styles.loaderContainer}>
-            <Loader minDisplayTime={2000} data-testid="loader" />
-          </div>
-        </section>
-      );
-    }
-
-    if (error) {
-      return (
-        <section className={styles.resultsSectionError}>
-          <p role="paragraph">{error}</p>
-        </section>
-      );
-    }
-
-    if (results.length === 0) {
-      return (
-        <p className={styles.noResults}>
-          No characters found. Try another search!
-        </p>
-      );
-    }
-
+const ResultsSection = ({ loading, error, results }: ResultsSectionProps) => {
+  if (loading) {
     return (
-      <section className={styles.resultsSection}>
-        <CardList characters={results} />
+      <section
+        className={styles.resultsSection}
+        role="alert"
+        aria-live="polite"
+      >
+        <div className={styles.loaderContainer}>
+          <Loader minDisplayTime={2000} data-testid="loader" />
+        </div>
       </section>
     );
   }
-}
+
+  if (error) {
+    return (
+      <section className={styles.resultsSectionError}>
+        <p role="paragraph">{error}</p>
+      </section>
+    );
+  }
+
+  if (results.length === 0) {
+    return (
+      <p className={styles.noResults}>
+        No characters found. Try another search!
+      </p>
+    );
+  }
+
+  return (
+    <section className={styles.resultsSection}>
+      <CardList characters={results} />
+    </section>
+  );
+};
 
 export default ResultsSection;
