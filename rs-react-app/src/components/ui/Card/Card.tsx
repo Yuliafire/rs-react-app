48666c7885f01ee3e072e3f849dc6358e5d94c0f
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-07-20 11:57:23 +0300

    refactor: refactor Code class component into functional

--- rs-react-app/src/components/ui/Card/Card.tsx
+++ rs-react-app/src/components/ui/Card/Card.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './Card.module.scss';
 import type { CharacterDetails } from '../../../types/types';
 
@@ -6,44 +5,40 @@ interface CardProps {
   character: CharacterDetails;
 }
 
-class Card extends React.Component<CardProps> {
-  render() {
-    const { character } = this.props;
-
-    return (
-      <div className={styles.card} data-testid="card">
-        <div className={styles.cardImage}>
-          <img src={character.image} alt={character.name} />
-          <span
-            className={`${styles.statusBadge} ${styles[character.status.toLowerCase()]}`}
-            data-testid="status-badge"
-          >
-            {character.status}
-          </span>
+const Card = ({ character }: CardProps) => {
+  return (
+    <div className={styles.card} data-testid="card">
+      <div className={styles.cardImage}>
+        <img src={character.image} alt={character.name} />
+        <span
+          className={`${styles.statusBadge} ${styles[character.status.toLowerCase()]}`}
+          data-testid="status-badge"
+        >
+          {character.status}
+        </span>
+      </div>
+      <div className={styles.cardContent}>
+        <h3>{character.name}</h3>
+        <div className={styles.details}>
+          <p>
+            <strong>Species:</strong> {character.species}
+          </p>
+          <p>
+            <strong>Gender:</strong> {character.gender}
+          </p>
+          <p>
+            <strong>Origin:</strong> {character.origin.name}
+          </p>
+          <p>
+            <strong>Location:</strong> {character.location.name}
+          </p>
         </div>
-        <div className={styles.cardContent}>
-          <h3>{character.name}</h3>
-          <div className={styles.details}>
-            <p>
-              <strong>Species:</strong> {character.species}
-            </p>
-            <p>
-              <strong>Gender:</strong> {character.gender}
-            </p>
-            <p>
-              <strong>Origin:</strong> {character.origin.name}
-            </p>
-            <p>
-              <strong>Location:</strong> {character.location.name}
-            </p>
-          </div>
-          <div className={styles.episodes}>
-            Episodes: {character.episode.length}
-          </div>
+        <div className={styles.episodes}>
+          Episodes: {character.episode.length}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;
