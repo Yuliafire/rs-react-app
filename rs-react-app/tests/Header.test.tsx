67b70901d22c73074c8bfc315077b1889ffce820
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-07-24 10:52:41 +0300

    refactor: modify Header test

--- rs-react-app/tests/Header.test.tsx
+++ rs-react-app/tests/Header.test.tsx
@@ -1,9 +1,19 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, it, expect } from 'vitest';
 import Header from '../src/components/layout/Header/Header';
-import { describe, it } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 
 describe('Header', () => {
   it('renders without crashing', () => {
-    render(<Header />);
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText('Rick and Morty Portal')).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /Main/i })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /About/i })).toBeInTheDocument();
   });
 });
