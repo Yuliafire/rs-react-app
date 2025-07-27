6247b7f1024191b06c18e02d7cef0b5edeef46cd
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-07-28 00:13:37 +0300

    refactor: modify Footer

--- rs-react-app/src/components/layout/Footer/Footer.tsx
+++ rs-react-app/src/components/layout/Footer/Footer.tsx
@@ -33,9 +33,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <p className={styles.copyright}>
-            © {new Date().getFullYear()} My App
-          </p>
+          <p className={styles.copyright}>© 2025 Lingua Voice</p>
           <div className={styles.links}>
             <a
               href="https://github.com/Yuliafire"
