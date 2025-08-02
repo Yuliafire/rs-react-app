b2911683fc17a26351b85936d8f3d7b8e06de8de
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-08-02 16:19:22 +0300

    refactor: import useTheme into NotFound page, update themes styles for component

--- rs-react-app/src/pages/not-found/Notfound.tsx
+++ rs-react-app/src/pages/not-found/Notfound.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './Notfound.module.scss';
+import { useTheme } from '../../hooks/useTheme';
 
 export const NotFound = () => {
+  const { theme } = useTheme();
   return (
-    <div className={styles.notFound}>
+    <div className={`${styles.notFound} ${styles[theme]}`}>
       <h2>04 - Page Not Found</h2>
       <p>The page you&apos;re looking for doesn&apos;t exist.</p>
       <Link to="/" className={styles.homeLink}>
