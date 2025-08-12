f55d59a3cdeb26a41757141109499b34651b4479
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-08-12 17:10:48 +0300

    refactor: migrate not-found page - use server

--- rs-react-app/my-app/src/pages/not-found/Notfound.tsx
+++ rs-react-app/my-app/src/pages/not-found/Notfound.tsx
@@ -1,14 +1,15 @@
-import { Link } from 'react-router-dom';
+'use server';
+import Link from 'next/link';
 import styles from './Notfound.module.scss';
-import { useTheme } from '../../shared/hooks/useTheme';
+// import { useTheme } from '../../shared/hooks/useTheme';
 
 export const NotFound = () => {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
   return (
-    <div className={`${styles.notFound} ${styles[theme]}`}>
+    <div className={`${styles.notFound}`}>
       <h2>404 - Page Not Found</h2>
       <p>The page you&apos;re looking for doesn&apos;t exist.</p>
-      <Link to="/" className={styles.homeLink}>
+      <Link href="/" className={styles.homeLink}>
         Return to Home
       </Link>
     </div>
